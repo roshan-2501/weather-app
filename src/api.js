60fa2335461fc27cb645aa4022a70805d5563289
roshan-2501 <roshant2513@gmail.com>
2024-08-05 15:52:37 +0530

    initial commit

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/places/%7BplaceId%7D/distance?toPlaceId=Q60';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '699252c6b7mshccb08f9b8d65badp1504d1jsn66b7a5a80b72',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
